4ba55a654370f9a9eafe156d4f7d4978a925c003
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-12 13:09:25 -0400

    all

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        printFactors(6); System.out.println( );
+        printFactors(32); System.out.println( );
+        printFactors(10); System.out.println( );
+        printFactors(-1); System.out.println( );
+
+
+    }
+
+    public static int printFactors(int number) {
+        if (number < 1){
+            return -1;
+        }
+
+        for(int i = 1; i <= number; i++){
+            if (number % i == 0){
+                System.out.print(i);
+            }
+            
+        }
+
+
+        return number;
+    }
 }
